4dc5e5cb49510b709e9af18779fa8b82e7d15444
mohit <mohitsasane76@gmail.com>
2024-07-22 11:54:36 +0530

    bug

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className='flex justify-between bg-slate-500 w-full  p-[1rem] text-white fixed top-0 z-40'>
+    <div className='flex justify-between bg-slate-500 w-full  p-[1rem] text-white fixed top-0 z-20'>
       <Link to={"/"} className="sm:ml-[5rem] ml-4 text-red-700  text-xl font-bold"><h1>Movie.com</h1></Link>
       <ul className='flex gap-[1rem] sm:mr-[5rem] mr-4'>
         <li><Link to={"/movie"}>Movie</Link></li>
